f1a339db782b8d284f7cb87588e26d1960dda937
Chris <chris@monoclesecurity.com>
2024-07-28 16:36:27 +0000

    fixing

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@
 #include <drm/drm_fourcc.h>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
+#include <fcntl.h>
 #include <fstream>
 #include <GLES3/gl3.h>
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 #include <iostream>
+#include <linux/dma-heap.h>
 #include <memory>
 #include <rga/im2d.hpp>
 #include <rga/im2d_buffer.h>
@@ -27,8 +29,63 @@
 #include <rockchip/mpp_buffer.h>
 #include <rockchip/mpp_packet.h>
 #include <rockchip/rk_mpi_cmd.h>
+#include <sys/ioctl.h>
 #include <thread>
 #include <vector>
+
+//TODO not sure which one to use
+#define DMA_HEAP_UNCACHE_PATH           "/dev/dma_heap/system-uncached"
+#define DMA_HEAP_PATH                   "/dev/dma_heap/system"
+#define DMA_HEAP_DMA32_UNCACHE_PATCH    "/dev/dma_heap/system-uncached-dma32"
+#define DMA_HEAP_DMA32_PATCH            "/dev/dma_heap/system-dma32"
+#define CMA_HEAP_UNCACHE_PATH           "/dev/dma_heap/cma-uncached"
+#define RV1106_CMA_HEAP_PATH	        "/dev/rk_dma_heap/rk-dma-heap-cma"
+
+int dma_buf_alloc(const char *path, size_t size, int *fd, void **va) {
+    int ret;
+    int prot;
+    void *mmap_va;
+    int dma_heap_fd = -1;
+    struct dma_heap_allocation_data buf_data;
+
+    /* open dma_heap fd */
+    dma_heap_fd = open(path, O_RDWR);
+    if (dma_heap_fd < 0) {
+        printf("open %s fail!\n", path);
+        return dma_heap_fd;
+    }
+
+    /* alloc buffer */
+    memset(&buf_data, 0x0, sizeof(struct dma_heap_allocation_data));
+
+    buf_data.len = size;
+    buf_data.fd_flags = O_CLOEXEC | O_RDWR;
+    ret = ioctl(dma_heap_fd, DMA_HEAP_IOCTL_ALLOC, &buf_data);
+    if (ret < 0) {
+        printf("RK_DMA_HEAP_ALLOC_BUFFER failed\n");
+        return ret;
+    }
+
+    /* mmap va */
+    if (fcntl(buf_data.fd, F_GETFL) & O_RDWR)
+        prot = PROT_READ | PROT_WRITE;
+    else
+        prot = PROT_READ;
+
+    /* mmap contiguors buffer to user */
+    mmap_va = (void *)mmap(NULL, buf_data.len, prot, MAP_SHARED, buf_data.fd, 0);
+    if (mmap_va == MAP_FAILED) {
+        printf("mmap failed: %s\n", strerror(errno));
+        return -errno;
+    }
+
+    *va = mmap_va;
+    *fd = buf_data.fd;
+
+    close(dma_heap_fd);
+
+    return 0;
+}
 
 int main()
 {
@@ -163,10 +220,45 @@ int main()
           const RK_U32 offset_y = mpp_frame_get_offset_y(source_frame);
           const RK_U32 hor_stride = mpp_frame_get_hor_stride(source_frame);
           const RK_U32 ver_stride = mpp_frame_get_ver_stride(source_frame);
-          const rga_buffer_handle_t dma_handle = importbuffer_fd(fd, size);
-          const rga_buffer_t dma_buffer = wrapbuffer_handle(dma_handle, width, height, RK_FORMAT_YCbCr_422_SP);
-std::cout << dma_handle << " " << dma_buffer.fd << " " << dma_buffer.width << " " << dma_buffer.height << std::endl;//TODO
+//          const rga_buffer_handle_t dma_handle = importbuffer_fd(fd, size);
+//          const rga_buffer_t dma_buffer = wrapbuffer_handle(dma_handle, width, height, RK_FORMAT_YCbCr_422_SP);
           // Create a destination DMA buffer
+//          const size_t dst_buffer_size = width * height * 3;
+
+
+//TODO
+//          int dst_dma_fd = 0;
+//          char* dst_buf = nullptr;
+//          if (dma_buf_alloc(DMA_HEAP_UNCACHE_PATH, dst_buffer_size, &dst_dma_fd, (void **)&dst_buf))
+          {
+//            std::cout << "Failed to allocate DMA destination buffer" << std::endl;
+//            return -10;
+          }
+          
+
+
+
+
+
+
+
+
+/*TODO          char* dst_buf = reinterpret_cast<char*>(malloc(dst_buffer_size));
+          rga_buffer_handle_t dst_handle = importbuffer_virtualaddr(dst_buf, dst_buffer_size);
+          if (dst_handle == 0)
+          {
+            std::cout << "Failed to create destination buffer" << std::endl;
+            return -10;
+          }
+          const rga_buffer_t dst_buffer = wrapbuffer_handle(dst_handle, width, height, RK_FORMAT_RGB_888);*/
+
+
+
+
+//std::cout << dma_handle << " " << dma_buffer.fd << " " << dma_buffer.width << " " << dma_buffer.height << std::endl;//TODO
+//std::cout << dst_buffer.fd << " " << dst_buffer.width << " " << dst_buffer.height << std::endl;//TODO
+
+
 
 //TODO
 
@@ -188,7 +280,8 @@ std::cout << dma_handle << " " << dma_buffer.fd << " " << dma_buffer.width << " 
             std::cout << "Invalid frame format" << std::endl;
             return -10;
           }
-          EGLAttrib atts[] = {
+//          EGLAttrib atts[] = {
+          EGLint atts[] = {
                                EGL_WIDTH, width,
                                EGL_HEIGHT, height,
                                EGL_LINUX_DRM_FOURCC_EXT, DRM_FORMAT_NV12,
@@ -196,11 +289,19 @@ std::cout << dma_handle << " " << dma_buffer.fd << " " << dma_buffer.width << " 
                                EGL_DMA_BUF_PLANE0_OFFSET_EXT, offset_x,
                                EGL_DMA_BUF_PLANE0_PITCH_EXT, hor_stride,
                                EGL_DMA_BUF_PLANE1_FD_EXT, fd,
-                               EGL_DMA_BUF_PLANE1_OFFSET_EXT, hor_stride * height,
+                               EGL_DMA_BUF_PLANE1_OFFSET_EXT, offset_x + (hor_stride * height),
                                EGL_DMA_BUF_PLANE1_PITCH_EXT, hor_stride,
+//                               EGL_LINUX_DRM_FOURCC_EXT, DRM_FORMAT_RGB888,
+//                               EGL_DMA_BUF_PLANE0_FD_EXT, dst_dma_fd,
+//                               EGL_DMA_BUF_PLANE0_OFFSET_EXT, 0,
+//                               EGL_DMA_BUF_PLANE0_PITCH_EXT, width,//TODO maybe pitch/stride?
                                EGL_NONE
                              };
-          const EGLImageKHR egl_image = eglCreateImage(glfwGetEGLDisplay(), glfwGetEGLContext(window), EGL_LINUX_DMA_BUF_EXT, nullptr, atts); // Fails to create image here!!!
+//          const EGLImageKHR egl_image = eglCreateImage(glfwGetEGLDisplay(), glfwGetEGLContext(window), EGL_LINUX_DMA_BUF_EXT, nullptr, atts); // Fails to create image here!!!
+
+//TODO
+          PFNEGLCREATEIMAGEKHRPROC eglCreateImageKHRt = (PFNEGLCREATEIMAGEKHRPROC)eglGetProcAddress("eglCreateImageKHR");
+          const EGLImageKHR egl_image = eglCreateImageKHRt(glfwGetEGLDisplay(), EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, nullptr, atts); // Fails to create image here!!!
           if (egl_image == EGL_NO_IMAGE_KHR)
           {
             std::cout << "Failed to create EGL image" << std::endl;
